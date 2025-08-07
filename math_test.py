d8e3f43e9df1db0a64ab46c7de71ff46e2be87e7
poluribalajisandeep16-cmyk <poluribalajisandeep16@gmail.com>
2025-08-07 16:59:48 +0530

    Update math_test.py

--- math_test.py
+++ math_test.py
@@ -1,11 +1,9 @@
 from file import file
 
-
 def test_file():
     assert file() == "hello,world!"
- 
 
-def test_math():
-    assert 2 + 2 == 4
-    assert 2 + 3 == 5
-    assert 2 + 4 == 6
+
+assert 2 + 2 == 4
+assert 2 + 3 == 5
+assert 2 + 4 == 6
